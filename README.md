92ab8e7ca3cc28a3aeffb7695444eae0f8432998
lojze.wang <lojze.wang@ly.com>
2023-02-24 18:18:25 +0800

    xiug

--- README.md
+++ README.md
@@ -36,3 +36,21 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+
+*   getStaticProps 静态页面预加载，因此它不适合频繁更新或根据每个用户请求更改的数据。
+
+*   getServerSideProps 适合每次都更新，比如用户操作更新数据，重新渲染页面
+
+>   因为getServerSideProps在请求时被调用，它的参数 ( context) 包含请求特定的参数。
+
+getServerSideProps仅当您需要预呈现其数据必须在请求时获取的页面时才应使用。到第一个字节 ( TTFB ) 的时间将比getStaticProps服务器必须计算每个请求的结果要慢，并且如果没有额外配置， CDN无法缓存结果。
+
+https://nextjs.org/learn/basics/data-fetching/request-time
+
+
+*   Client-side Rendering 客户端渲染：静态生成（预呈现）不需要外部数据的页面部分。当页面加载时，使用 JavaScript 从客户端获取外部数据并填充其余部分。
+
+> 此方法适用于用户仪表板页面。因为仪表板是私有的、特定于用户的页面，所以 SEO 不相关，并且页面不需要预呈现。数据更新频繁，需要请求时取数据。
+SWR:用于数据请求的React Hooks 库。数据获取React 钩子。如果您在客户端获取数据，我们强烈推荐它。它处理缓存、重新验证、焦点跟踪、间隔重新获取等。//https://swr.vercel.app/
+
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
+import Layout, { siteTitle } from '@/components/layout';
+import utilStyles from '@/styles/utils.module.css';
+import { getSortedPostsData } from '@/lib/posts';
 
-export default function Home() {
+
+export default function Home({allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -14,7 +16,41 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
 }
+
+// getStaticProps 函数只会在服务端运行/多个直接写在里面
+// 多个外部api，划分组件比较好，组件需要设计的合理，比如结合api
+// 只能在页面导出
+// 由于静态生成在构建时发生一次，因此它不适合频繁更新或根据每个用户请求更改的数据。
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+// 服务端渲染
+
+// export async function getServerSideProps(context) {
+//   return {
+//     props: {
+//       // props for your component
+//     },
+//   };
+// }
