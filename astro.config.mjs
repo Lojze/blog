b15aeff20d05a17b629f1be506cb813b8cc0e71c
lojze.wang <lojze.wang@ly.com>
2023-12-09 15:10:01 +0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   // site: "https://example.com"
   site: "http://10.181.16.47:4321/",
-  integrations: [preact()]
+  integrations: [preact(), tailwind()]
 });
