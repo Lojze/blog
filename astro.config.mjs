5b43ffce88b9e186bd7c52bdf2bc875b797ea86a
lojze.wang <lojze.wang@ly.com>
2023-12-11 14:26:44 +0800

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
-
+import deno from '@astrojs/deno';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: deno(),
   // site: "https://example.com"
   site: "https://lojzebolg.deno.dev/",
   integrations: [preact(), tailwind()]
