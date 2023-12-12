f04c16aa531bfef3e013297c3203f789727db34c
lojze.wang <lojze.wang@ly.com>
2023-12-12 11:25:17 +0800

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
-
 import deno from "@astrojs/deno";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   // site: "https://example.com"
-  site: "https://lojzebolg.deno.dev/", // import.meta.url
-  integrations: [preact(), tailwind()]
+  site: "https://lojzebolg.deno.dev",
+  // import.meta.url
+  integrations: [preact(), tailwind(), sitemap()]
 });
